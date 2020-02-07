d97bbdd72a3036e4e8940255673f793c79e1016b
Dayvon Allen <dayvon@107-144-156-227.biz.spectrum.com>
2020-02-07 18:49:11 -0500

    test

--- src/components/register-component/RegisterComponent.tsx
+++ src/components/register-component/RegisterComponent.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 export class RegisterComponent extends React.Component{
     render(){
         return(
-            <div></div>
+            <div>
+                <h1>Hello World!</h1>
+            </div>
         )
     }
 }
